fb9816f4bec6304b7de5182193ffc102a1bef1c4
Hailey Martin <luciddragon@gmail.com>
2015-05-30 12:13:11 +1000

    Prolog: Find minimum value in a list.

--- Prolog/min.pl
+++ Prolog/min.pl
@@ -1,3 +1,3 @@
-min([X|[]], X).
-min([X,Y], X) :- X < Y.
-min([X,Y], Y) :- Y =< X.
+min([X], X).
+min([Head|Tail], Head) :- min(Tail, TailMin), Head =< TailMin.
+min([Head|Tail], TailMin) :- min(Tail, TailMin), TailMin < Head.
