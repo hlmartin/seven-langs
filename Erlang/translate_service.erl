0c66443ec3bf9987cca7c556b28dcc0ca5c12ca6
Hailey Martin <luciddragon@gmail.com>
2015-09-27 22:30:34 +1000

    Day 3 Erlang example questions

---
+++ Erlang/translate_service.erl
@@ -0,0 +1,23 @@
+-module(translate_service).
+-export([loop/0, translate/2]).
+
+loop() ->
+  receive
+    {From, "casa"} ->
+      From ! "house",
+      loop();
+    {From, "blanca"} ->
+      From ! "white",
+      loop();
+    {From, "kill"} ->
+      exit({translate_service, die, at, erlang:time()});
+    {From, _} ->
+      From ! "I don't understand.",
+      loop()
+  end.
+
+translate(To, Word) ->
+  To ! {self(), Word},
+  receive
+    Translation -> Translation
+  end.
